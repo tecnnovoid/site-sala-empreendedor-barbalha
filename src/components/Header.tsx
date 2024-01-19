486c8f2f00ff1779a8596fcc29d55af524987940
livia araujo <liviaaraujo.dev@gmail.com>
2024-01-19 11:43:25 -0300

    create home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,10 +32,10 @@ export function Header() {
     }
 
     return (
-        <header className="flex-col items-center justify-center fixed top-0 left-0 right-0 z-50 md:flex md:flex-row md:justify-between md:pr-24 md:pl-10">
+        <header className={openMenu ? "flex-col h-[100vh] items-center justify-center bg-white fixed top-0 left-0 right-0 z-50 lg:flex lg:flex-row lg:justify-between lg:pr-24 md:pl-10" :"flex-col items-center justify-center bg-white fixed top-0 left-0 right-0 z-50 lg:flex lg:flex-row lg:justify-between lg:pr-24 lg:pl-10"} >
             <div className="flex items-center justify-between px-6 ">
                 <img src={logo} alt="" width={250} />
-                <button onClick={onClickOpenMenu} className="md:hidden">
+                <button onClick={onClickOpenMenu} className="lg:hidden">
                     {
                         !openMenu ?  <GiHamburgerMenu size={28} /> : <IoClose size={30} />
 
@@ -45,8 +45,8 @@ export function Header() {
 
             </div>
                 
-                <nav className={openMenu ? "flex md:flex" : "hidden md:flex "}>
-                    <ul className="flex-col ml-8 md:flex md:flex-row md:gap-10">
+                <nav className={openMenu ? "flex lg:flex" : "hidden lg:flex "}>
+                    <ul className="flex-col ml-8 lg:flex lg:flex-row lg:gap-10">
                         {
                             navItems.map((item) => <li className="pt-8">
                                 <a href="" className="text-2xl font-medium">{item.text}</a>
