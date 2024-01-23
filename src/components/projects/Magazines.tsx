9757c54995cc3b1811fc2f02951f8dcf7dc72f9c
livia araujo <liviaaraujo.dev@gmail.com>
2024-01-22 21:25:35 -0300

    fix projects

--- src/components/projects/Magazines.tsx
+++ src/components/projects/Magazines.tsx
@@ -13,7 +13,7 @@ export function Magazines() {
 
                 </p>
                 <a className="bg-[#02B1C8] font-semibold text-white text-xl w-56 rounded-xl py-3 flex items-center justify-center mt-4">
-                    Acessar revistas
+                    Acessar revistas 
                 </a>
             </div>
             <img src={magazinesImg} alt="Revistas da Sala do Empreendedor de barbalha" className="w-[95%] lg:w-[45%]" />
