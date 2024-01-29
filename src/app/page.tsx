f641fd21d29567a8b85d2bc92594929ee888fa76
livia araujo <liviaaraujo.dev@gmail.com>
2024-01-29 08:49:05 -0300

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <Home/>
@@ -18,6 +18,6 @@ export default function App() {
         <Projects />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
